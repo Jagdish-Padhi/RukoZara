3dcadcc421f343c78d3c2ca061efe413812253b6
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-01-16 07:00:07 +0000

    refactor(frontend): simplify App as clean entry point

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import './App.css'
+import './index.css';
+import './styles/global.css';
+import { Home } from './pages/Home';
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+  return <Home />;
 }
 
-export default App
+export default App;
